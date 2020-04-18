f7953d6a2e3168f44772fb69e761aeb7093fdf86
Eyal Levin <eyalev@gmail.com>
2020-04-18 10:21:10 +0300

    Update

---
+++ setup_bash_commands.sh
@@ -0,0 +1,22 @@
+
+SHELL_COMMANDS_DIR=~/.shell-commands
+
+# mkdir -p $SHELL_COMMANDS_DIR
+
+LOG_FILE=~/.shell-commands-log
+
+echo "[$(date +'%Y-%m-%dT%H:%M:%S')] [START] setup_bash_commands" >> $LOG_FILE
+echo "[$(date +'%Y-%m-%dT%H:%M:%S')] [START] PWD: ${PWD}" >> $LOG_FILE
+
+git clone git@github.com:eyalev/shell-history.git $SHELL_COMMANDS_DIR
+
+touch ~/.bash_history
+
+TEMP_HISTORY=/tmp/bash_history_temp
+
+cat $SHELL_COMMANDS_DIR/bash_commands > $TEMP_HISTORY
+cat ~/.bash_history >> $TEMP_HISTORY
+mv $TEMP_HISTORY ~/.bash_history
+
+echo "[$(date +'%Y-%m-%dT%H:%M:%S')] [END] setup_bash_commands" >> $LOG_FILE
+
